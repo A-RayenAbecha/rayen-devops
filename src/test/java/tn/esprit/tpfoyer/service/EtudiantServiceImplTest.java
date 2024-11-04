c3668109c3f6e1694d641e71c1b6c692679a8fb6
A-RayenAbecha <ahmedrayen.abecha@esprit.tn>
2024-11-04 20:12:46 +0100

    modif testM2

--- src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplTest.java
@@ -24,7 +24,7 @@ class EtudiantServiceImplTest {
   EtudiantServiceImpl es;
 
 
-  Etudiant etudiant = new Etudiant("Fertani", "Youssef", 12345678, new Date(2000, 8, 23));
+  Etudiant etudiant = new Etudiant("Abecha", "AhmedRayen", 12345678, new Date(2000, 2, 26));
     List<Etudiant> etudiants = new ArrayList<>();
 
 
